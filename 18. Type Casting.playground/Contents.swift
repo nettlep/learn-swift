8df444743eb9840f15c67ba7c728cae9fa133051
Sarah Allen <sarah@ultrasaurus.com>
2015-12-26 15:11:16 -0800

    upgrade 18.Type Casting & fix syntax errors

--- 18. Type Casting.playground/Contents.swift
+++ 18. Type Casting.playground/Contents.swift
@@ -2,11 +2,10 @@
 // Things to know:
 //
 // * Type casting allows us to check the type of an instance and/or treat that instance as a
-//   different type from somewhere else in its own hieararchy.
+//   different type from somewhere else in its own hierarchy.
 //
 // * Type casting also allows us to determine if a type conforms to a protocol.
 //
-// * Additionally we can create
 // ------------------------------------------------------------------------------------------------
 
 // Let's start by creating a few types to work with:
@@ -115,19 +114,19 @@ let someObjects: [AnyObject] =
 ]
 
 // Here, we know that someObjects[] only contains Movie instances, so we'll use our forced version
-// of the typecast operator, "as". Note, however, that if somebody modifies the code later and adds
+// of the typecast operator: "as!". Note, however, that if somebody modifies the code later and adds
 // an instance of a non-Movie type (which they can do), we'll crash. This is why it's important
 // to limit our use of AnyObject and Any to only those cases where we absolutely need it.
 //
 // Let's see how we would use the someObjects array:
 for object: AnyObject in someObjects
 {
-	let movie = object as Movie
+	let movie = object as! Movie
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
 }
 
 // Alternatively, we can downcast the array itself rather than each item:
-var someMovies = someObjects as [Movie]
+var someMovies = someObjects as! [Movie]
 for movie in someMovies
 {
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
@@ -135,12 +134,12 @@ for movie in someMovies
 
 // Finally, we can avoid the additional local variable and performt he downcast right inside
 // the loop structure:
-for movie in someObjects as [Movie]
+for movie in someObjects as! [Movie]
 {
 	"Movie: '\(movie.name)' was directed by \(movie.director)"
 }
 
-// Any allows us store references to any type at all (not including functions), which can include
+// "Any" allows us store references to any type at all (not including functions), which can include
 // integers, floating points, strings or objects.
 //
 // Let's see this in action. We'll create an array of type Any[] and fill it with random bits and
