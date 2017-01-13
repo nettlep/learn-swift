b32d4011f8b5085d9b2af70471a94bceedb8543d
Van Simmons <rvsrvs@gmail.com>
2017-01-13 09:53:55 -0500

    Making the Map/Reduce/Filter stuff work

--- 25. Map Reduce and Filter.playground/Contents.swift
+++ 25. Map Reduce and Filter.playground/Contents.swift
@@ -34,7 +34,7 @@ class IntegerClass: Hashable, IntDescribable {
     var integerValue = 0
     var hashValue: Int { return integerValue }
     var description: String {
-            return "Instance of \(type(of: self))"
+        return "Instance of \(type(of: self))"
     }
     required init(_ withValue: Int) { integerValue = withValue }
 }
@@ -51,21 +51,25 @@ type(of: d)
 
 var m1 = d
 type(of: m1)
+m1
     
 var m2 = d
     .filter { (k,v) in return v is [IntDescribable] }
 type(of: m2)
+m2
     
 var m3 = d
     .filter { (k,v) in return v is [IntDescribable] }
     .map { (k,v) in return v as! [IntDescribable] }
 type(of: m3)
+m3
     
 var m4 = d
     .filter { (k,v) in return v is [IntDescribable] }
     .map { (k,v) in return v as! [IntDescribable] }
     .flatMap { $0 }
 type(of: m4)
+m4
     
 var m5 = d
     .filter { (k,v) in return v is [IntDescribable] }
@@ -73,6 +77,7 @@ var m5 = d
     .flatMap { $0 }
     .sorted { $0.integerValue > $1.integerValue }
 type(of: m5)
+m5
 
 var m6 = d
     .filter { (k,v) in return v is [IntDescribable] }
@@ -81,6 +86,7 @@ var m6 = d
     .sorted { $0.integerValue > $1.integerValue }
     .map { IntegerClass($0.integerValue) }
 type(of: m6)
+m6
 
 var m7 = d
     .filter { (k,v) in return v is [IntDescribable] }
@@ -90,21 +96,26 @@ var m7 = d
     .map { IntegerClass($0.integerValue) }
     .reduce(0) { return $0 + $1.integerValue }
 type(of: m7)
+m7
 
 var m8 = Set<IntegerClass>(m6)
 type(of: m8)
+m8
 
 var m9 = Set<IntegerClass>(m6)
     .map { return $0.integerValue }
 type(of: m9)
+m9
 
 var m10 = Set<IntegerClass>(m6)
     .map { return $0.integerValue }
     .sorted { $0 < $1 }
 type(of: m10)
+m10
 
 var m11 = Set<IntegerClass>(m6)
     .map { return $0.integerValue }
     .sorted { $0 < $1 }
     .reduce(1) { return $0 * $1 }
 type(of: m11)
+m11
